c11b0682cfb3cc8da6578c11ec4ad1c118d54756
bharath <bharaththermal@gmail.com>
2021-12-14 17:33:14 +0530

    vvvv

--- scripts/git_update.sh
+++ scripts/git_update.sh
@@ -23,6 +23,7 @@ echo "Current Version: $CURRENT_VERSION"
 # replace . with space so can split into an array
 CURRENT_VERSION_PARTS=(${CURRENT_VERSION//./ })
 
+
 # get number parts
 VNUM1=${CURRENT_VERSION_PARTS[0]}
 # VNUM2=${CURRENT_VERSION_PARTS[1]}
