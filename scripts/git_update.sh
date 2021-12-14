e5f93178230acdba97d4285b9fb396f03664fc92
bharath <bharaththermal@gmail.com>
2021-12-14 17:31:19 +0530

    vvv

--- scripts/git_update.sh
+++ scripts/git_update.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-VERSION="major"
+VERSION=""
 
 # get parameters
 while getopts v: flag
 do
   case "${flag}" in
-    v) VERSION=${OPTARG};;
+v) VERSION=${OPTARG};;
   esac
 done
 
@@ -25,25 +25,26 @@ CURRENT_VERSION_PARTS=(${CURRENT_VERSION//./ })
 
 # get number parts
 VNUM1=${CURRENT_VERSION_PARTS[0]}
-VNUM2=${CURRENT_VERSION_PARTS[1]}
-VNUM3=${CURRENT_VERSION_PARTS[2]}
+# VNUM2=${CURRENT_VERSION_PARTS[1]}
+# VNUM3=${CURRENT_VERSION_PARTS[2]}
 
-if [[ $VERSION == 'major' ]]
-then
+# if [[ $VERSION == 'major' ]]
+# then
   VNUM1=v$((VNUM1+1))
-elif [[ $VERSION == 'minor' ]]
-then
-  VNUM2=$((VNUM2+1))
-elif [[ $VERSION == 'patch' ]]
-then
-  VNUM3=$((VNUM3+1))
-else
-  echo "No version type (https://semver.org/) or incorrect type specified, try: -v [major, minor, patch]"
-  exit 1
-fi
+# elif [[ $VERSION == 'minor' ]]
+# then
+#   VNUM2=$((VNUM2+1))
+# elif [[ $VERSION == 'patch' ]]
+# then
+#   VNUM3=$((VNUM3+1))
+# else
+#   echo "No version type (https://semver.org/) or incorrect type specified, try: -v [major, minor, patch]"
+#   exit 1
+# fi
 
 # create new tag
-NEW_TAG="$VNUM1.$VNUM2.$VNUM3"
+NEW_TAG="$VNUM1"
+# NEW_TAG="$VNUM1.$VNUM2.$VNUM3"
 echo "($VERSION) updating $CURRENT_VERSION to $NEW_TAG"
 
 # get current hash and see if it already has a tag
